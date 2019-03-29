bbc33e6e186ab5ebe50ff6ed6978e0e23b5e0309
Koh Meng Wang <mengwangk@gmail.com>
2019-03-29 23:09:58 +0800

    Initial commit

---
+++ hello_world/target/debug/deps/hello_world-b049b7ef4e83aea2.d
@@ -0,0 +1,5 @@
+/Users/mengwangk/workspace/development/rust-lang/hello_world/target/debug/deps/hello_world-b049b7ef4e83aea2: src/main.rs
+
+/Users/mengwangk/workspace/development/rust-lang/hello_world/target/debug/deps/hello_world-b049b7ef4e83aea2.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/mengwangk/workspace/development/rust-lang/hello_world/target/debug/hello_world.dSYM: /Users/mengwangk/workspace/development/rust-lang/hello_world/src/main.rs
