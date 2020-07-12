0a5b5437d83683e7122030940b24869a1f22f553
Koh Meng Wang <mengwangk@gmail.com>
2020-07-12 10:06:42 +0800

    Initial commit

---
+++ learn/target/debug/deps/learn-085497ffd587c531.d
@@ -0,0 +1,5 @@
+/Users/mengwangk/workspace/development/rust-lang/learn/target/debug/deps/learn-085497ffd587c531.rmeta: src/main.rs
+
+/Users/mengwangk/workspace/development/rust-lang/learn/target/debug/deps/learn-085497ffd587c531.d: src/main.rs
+
+src/main.rs:
---
+++ learn/target/debug/deps/main-b2ed26cc874654e9.d
@@ -0,0 +1,5 @@
+/Users/mengwangk/workspace/development/rust-lang/learn/target/debug/deps/main-b2ed26cc874654e9.rmeta: src/main.rs
+
+/Users/mengwangk/workspace/development/rust-lang/learn/target/debug/deps/main-b2ed26cc874654e9.d: src/main.rs
+
+src/main.rs:
