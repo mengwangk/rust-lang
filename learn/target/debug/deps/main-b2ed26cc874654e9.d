8493e94b8f739177186487693306495baf08713b
Koh Meng Wang <mengwangk@gmail.com>
2020-07-12 10:09:07 +0800

    Initial commit

--- learn/target/debug/deps/main-b2ed26cc874654e9.d
+++
@@ -1,5 +0,0 @@
-/Users/mengwangk/workspace/development/rust-lang/learn/target/debug/deps/main-b2ed26cc874654e9.rmeta: src/main.rs
-
-/Users/mengwangk/workspace/development/rust-lang/learn/target/debug/deps/main-b2ed26cc874654e9.d: src/main.rs
-
-src/main.rs:
